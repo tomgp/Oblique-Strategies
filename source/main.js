ae00c05d2ff927885881059c89d56b25fa16cec7
tomgp <tom.g.pearson@gmail.com>
2015-12-11 11:19:24 +0000

    data loading

--- source/main.js
+++ source/main.js
@@ -1,4 +1,23 @@
 'use strict';
 require('babelify/polyfill'); //remove this line if you don't care about ES6 pollyfils
+var d3 = {
+	request:require('d3-request'),
+	select:require('d3-selection').select
+};
+var request = require('d3-request');
+var d3select = require('d3-selection');
 
-console.log('Hey! Ho! Let\'s go!'); //remove this line if you don't care about The Ramones
+
+d3.request.csv('data/obliquestrategies.csv')
+	.get(function(error, data){
+
+		d3.select('article')
+			.selectAll('section.card')
+			.data(data)
+				.enter()
+			.append('section').attr('class','card hidden')
+				.text(function(d){
+					return d.card_text;
+				});
+	
+	});
