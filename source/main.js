d523fa5f5e1d4c8a7554dc101c027b3e3d6cf80c
tomgp <tom.g.pearson@gmail.com>
2015-12-11 17:01:13 +0000

    styling text etc

--- source/main.js
+++ source/main.js
@@ -1,23 +1,58 @@
 'use strict';
-require('babelify/polyfill'); //remove this line if you don't care about ES6 pollyfils
+
 var d3 = {
-	request:require('d3-request'),
+	csv:require('d3-request').csv,
 	select:require('d3-selection').select
 };
-var request = require('d3-request');
-var d3select = require('d3-selection');
 
-
-d3.request.csv('data/obliquestrategies.csv')
-	.get(function(error, data){
-
-		d3.select('article')
-			.selectAll('section.card')
-			.data(data)
-				.enter()
-			.append('section').attr('class','card hidden')
-				.text(function(d){
-					return d.card_text;
-				});
-	
+d3.csv('data/obliquestrategies.csv')
+	.get(function(error, csvData){
+		draw(csvData, 5);
 	});
+
+function draw(data, displaySet){
+	if(displaySet !== undefined) displaySet = 5;
+
+	var filtered = data.filter(function(d){
+		return Number(d.edition) === displaySet;
+	});
+
+	d3.select('button.random').on('click',function(){
+		goTo( Math.floor( Math.random()*filtered.length ) );
+	});
+
+	d3.select('button.info').on('click',function(){
+		document.getElementById('info').scrollIntoView();
+	});
+
+
+	var dataJoin = d3.select('article')
+		.selectAll('section.card')
+		.data(filtered);
+	
+	dataJoin
+		.enter()
+		.append('section')
+		.attr('class','card unselected');
+	
+	dataJoin
+		.attr('id',function(d,i){ return 'card-'+i; })
+		.attr('class', function(d,i){
+			return 'card unselected';
+		})
+		.text(function(d){
+			if(d.card_text === '[blank white card]') return '';
+			return d.card_text;
+		});
+
+	goTo( Math.floor( Math.random()*filtered.length ) );
+}
+
+function goTo(id){
+	var card = d3.select('#card-'+id)
+	card.node().scrollIntoView();
+	d3.select('article').selectAll('.card').attr('class', function(d,i){
+		if(i===id) return 'card selected'
+		return 'card unselected';
+	})
+}
